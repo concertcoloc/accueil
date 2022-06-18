9a6d6065bde742e158952b4f12d289b9137e5658
unknown <gregoire.pellenc805@gmail.com>
2022-06-18 12:07:29 -0800

    ajout site

---
+++ forms/contact.php
@@ -0,0 +1,91 @@
+<?php
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$sujet = $_POST['subject'];
+$msg = $_POST['message'];
+
+var_dump($name);
+var_dump($email);
+var_dump($sujet);
+var_dump($msg);
+
+$to      = 'nobody@example.com';
+$subject = 'the subject';
+$message = 'hello';
+$headers = array(
+    'From' => 'webmaster@example.com',
+    'Reply-To' => 'webmaster@example.com',
+    'X-Mailer' => 'PHP/' . phpversion()
+);
+
+// mail($to, $subject, $message, $headers);
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  /**
+  * Requires the "PHP Email Form" library
+  * The "PHP Email Form" library is available only in the pro version of the template
+  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
+  * For more info and help: https://bootstrapmade.com/php-email-form/
+  */
+/*
+  // Replace contact@example.com with your real receiving email address
+  $receiving_email_address = 'gregoire.pellenc504@gmail.com';
+
+  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+    include( $php_email_form );
+  } else {
+    die( 'Unable to load the "PHP Email Form" Library!');
+  }
+
+  $contact = new PHP_Email_Form;
+  $contact->ajax = true;
+  
+  $contact->to = $receiving_email_address;
+  $contact->from_name = $_POST['name'];
+  $contact->from_email = $_POST['email'];
+  $contact->subject = $_POST['subject'];
+  
+  */
+
+  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  /*
+  $contact->smtp = array(
+    'host' => 'example.com',
+    'username' => 'example',
+    'password' => 'pass',
+    'port' => '587'
+  );
+  
+  
+  
+
+  $contact->add_message( $_POST['name'], 'From');
+  $contact->add_message( $_POST['email'], 'Email');
+  $contact->add_message( $_POST['message'], 'Message', 10);
+
+  echo $contact->send();
+ */
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+?>
